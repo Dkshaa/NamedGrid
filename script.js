b4b760edc4d9a5231e7686b5723e98a4d7e26085
Dkshaa <diksha98620@gmail.com>
2026-06-08 15:32:37 -0700

    Show active grid template

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const grid = document.querySelector("[data-grid]");
 const buttons = document.querySelectorAll("[data-layout]");
+const templateCode = document.querySelector("[data-template-code]");
+
+const templates = {
+  dashboard: `"header header header"
+"sidebar content1 content1"
+"sidebar content2 content3"
+"footer footer footer"`,
+  article: `"header header"
+"sidebar content1"
+"content2 content1"
+"content3 footer"`,
+  gallery: `"header header sidebar"
+"content1 content2 sidebar"
+"content1 content3 content3"
+"footer footer footer"`,
+};
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const layout = button.dataset.layout;
 
     grid.className = `container layout-${layout}`;
+    templateCode.textContent = templates[layout];
 
     buttons.forEach((item) => {
       item.classList.toggle("active", item === button);
